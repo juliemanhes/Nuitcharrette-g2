6069bde929c36dee9e05ecd9257eddd621506366
Anathea <solene.fernandez-serrano@hetic.net>
2017-01-07 01:56:55 +0100

    Ajout gestion curseur
    Curseur qui se déplace avec la musique, déplacement au clic

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@
 	var player = new Player();
 
 	player.registerPlayPauseButton('playpause');
+	player.registerTimelineAndCursor('timeline', 'cursor');
 	player.setMusic('audio/0.m4a');
 })();
--- js/player.class.js
+++ js/player.class.js
@@ -6,11 +6,10 @@
 
 function Player() {
 	this.audio = document.createElement('audio');
-	this.audio.autoplay = "";
+	this.audio.autoplay = '';
 
 	this.audio_source = document.createElement('source');
 	this.audio.appendChild(this.audio_source);
-
 }
 
 Player.prototype.registerPlayPauseButton = function(on_off) {
@@ -19,6 +18,34 @@ Player.prototype.registerPlayPauseButton = function(on_off) {
 			this.audio.play();
 		else
 			this.audio.pause();
+	};
+};
+
+Player.prototype.registerTimelineAndCursor = function(timeline, cursor) {
+	elem = document.getElementById(timeline);
+	elem.pressed = false;
+
+	elem.onmousemove = (evt) => {
+		if(!elem.pressed)
+			return;
+
+		var rect = evt.target.getBoundingClientRect();
+		var norm = (evt.clientX - rect.left) / rect.width;
+
+		this.audio.currentTime = norm * this.audio.duration;
+	};
+	elem.onmousedown = (evt) => {
+		elem.pressed = true;
+		elem.onmousemove(evt);
+	};
+	elem.onmouseup = () => {
+		elem.pressed = false;
+	};
+
+	this.audio.ontimeupdate = () => {
+		var cursorline = document.getElementById(cursor);
+		
+		cursorline.style['margin-left'] = (100 * this.audio.currentTime / this.audio.duration) + '%';
 	};
 };
 
